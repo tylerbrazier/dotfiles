512f0a86e86ee93b6c1caa112d128d9152e7754b
Tyler Brazier <tyler@tylerbrazier.com>
2015-04-22 13:50:23 -0400

    dotfiles are not hidden in project directory
    Removed the dot at the beginning of each file for visibility.
    mklinks.sh will now just make link names that use the basename of the
    file with a prepended dot. (Use the basename in case we want to link
    to files that exist in subdirectories of the project later.)

--- mklinks.sh
+++ mklinks.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
+set -e
 
 # Creates symlinks from home to each dotfile.
 
+# Each of these should exist within the directory of this script.
+# For each, a symlink will be created in your home directory with a name
+# that is the basename of the file with a prepended dot.
 dotfiles=(
-.bash_profile
-.bashrc
-.gitconfig
-.tmux.conf
-.vimrc
+  bash_profile
+  bashrc
+  gitconfig
+  tmux.conf
+  vimrc
 )
 
 cd $(dirname $0)
 
 for file in ${dotfiles[@]}; do
-  if [[ -e $HOME/$file ]] && [[ ! -h $HOME/$file ]]; then
-    echo "$HOME/$file already exists; skipping"
+  name="$HOME/.$(basename $file)"
+  if [ -e "$name" ] && [ ! -h "$name" ]; then
+    echo "$name already exists; skipping"
   else
-    echo "Linking $file"
-    ln -sfn $(readlink -f $file) $HOME/$file
+    target=$(readlink -f "$file")
+    echo "Linking $name -> $target"
+    ln -sfn "$target" "$name"
   fi
 done
