a13e0b6f43091b0f9b018bfe2fbe0749bdff9c2f
Tyler Brazier <tyler@tylerbrazier.com>
2017-11-06 13:49:38 -0600

    Make scripts/ and move install.sh into it
    I plan on adding more scripts to this project like keygen.sh to make ssh
    keys

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,20 +4,20 @@
 # Pass -f to overwrite existing dotfiles.
 # Pass -p to also install vim plugins.
 
-# in case the script was called from another dir
-cd "$(dirname "$0")"
+# make sure we're in the root of the project
+cd "$(dirname "$0")/.."
 
 [[ " $@ " == *" -f "* ]] && cmd="ln -svf" || cmd="ln -sv"
 
-$cmd "$(pwd)/bash_profile" ~/.bash_profile
-$cmd "$(pwd)/bashrc" ~/.bashrc
-$cmd "$(pwd)/gitconfig" ~/.gitconfig
-$cmd "$(pwd)/tmux.conf" ~/.tmux.conf
-$cmd "$(pwd)/vimrc" ~/.vimrc
-$cmd "$(pwd)/gvimrc" ~/.gvimrc
+eval $cmd "$(realpath bash_profile)" ~/.bash_profile
+eval $cmd "$(realpath bashrc)" ~/.bashrc
+eval $cmd "$(realpath gitconfig)" ~/.gitconfig
+eval $cmd "$(realpath tmux.conf)" ~/.tmux.conf
+eval $cmd "$(realpath vimrc)" ~/.vimrc
+eval $cmd "$(realpath gvimrc)" ~/.gvimrc
 
 mkdir -p ~/.config/nvim/
-$cmd "$(pwd)/init.vim" ~/.config/nvim/init.vim
+eval $cmd "$(realpath config/nvim/init.vim)" ~/.config/nvim/init.vim
 
 if [[ " $@ " == *" -p "* ]]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
