2015bd46beab3bf3c2041433fc6c050cd4259e36
Tyler Brazier <tyler@tylerbrazier.com>
2017-11-07 00:09:14 -0600

    Add initial xinitrc, Xresources, default i3 config
    Switching from gnome to i3 because it's lightweight, clean, and well
    documented, a lot like Arch linux.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Run `./scripts/install.sh -f -p` to make symlinks in home to dotfiles.
 
 - `-f` to overwrite existing files in home.
+- `-g` to include config files for gui
 - `-p` to also install vim plugins.
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 
 # Make symlinks in home to dotfiles.
 # Pass -f to overwrite existing dotfiles.
+# Pass -g to include config files for gui
 # Pass -p to also install vim plugins.
 
 # make sure we're in the root of the project
@@ -14,10 +15,18 @@ eval $cmd "$(realpath bashrc)" ~/.bashrc
 eval $cmd "$(realpath gitconfig)" ~/.gitconfig
 eval $cmd "$(realpath tmux.conf)" ~/.tmux.conf
 eval $cmd "$(realpath vimrc)" ~/.vimrc
-eval $cmd "$(realpath gvimrc)" ~/.gvimrc
 
 mkdir -p ~/.config/nvim/
 eval $cmd "$(realpath config/nvim/init.vim)" ~/.config/nvim/init.vim
+
+if [[ " $@ " == *" -g "* ]]; then
+  eval $cmd "$(realpath xinitrc)" ~/.xinitrc
+  eval $cmd "$(realpath Xresources)" ~/.Xresources
+  eval $cmd "$(realpath gvimrc)" ~/.gvimrc
+
+  mkdir -p ~/.config/i3/
+  eval $cmd "$(realpath config/i3/config)" ~/.config/i3/config
+fi
 
 if [[ " $@ " == *" -p "* ]]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
