3751157848086e4e5e4019ae165be2237fc15aa8
Tyler Brazier <tyler@tylerbrazier.com>
2018-02-03 16:35:09 -0600

    superuseradd.sh: write config to sudoers.d/custom

--- scripts/superuseradd.sh
+++ scripts/superuseradd.sh
@@ -2,14 +2,17 @@
 set -e
 
 test $(id -u) -ne 0 && echo 'Run as root' >&2 && exit 1
+test ! -d /etc/sudoers.d && echo 'Make sure sudo is installed' >&2 && exit 1
 
 read -p 'New user: ' user
-
 test -z "$user" && exit 1
 id "$user" >/dev/null 2>&1 && echo "$user already exists" && exit 1
 
 useradd -m -g wheel -s /bin/bash "$user"
 passwd "$user"
 
-# allow member of wheel group to sudo without password
-sed -i~ 's/^# *\(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
+echo 'Writing to /etc/sudoers.d/custom:'
+tee /etc/sudoers.d/custom <<EOF
+# allow members of wheel group to sudo without password
+%wheel ALL=(ALL) NOPASSWD: ALL
+EOF
