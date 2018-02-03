02da6cdcd772546ac3afef0df9f6bf35607d7849
Tyler Brazier <tyler@tylerbrazier.com>
2018-02-03 16:42:37 -0600

    packages.sh: make packages commented by default
    To prevent accidental installation

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,61 +4,61 @@ set -e
 test $(id -u) -ne 0 && echo 'Run as root' >&2 && exit 1
 
 cat >/tmp/packages <<PACKAGES
-# essentials
-vim
-git
-sudo
-tmux
-openssh
-bash-completion
+### essentials
+#vim
+#git
+#sudo
+#tmux
+#openssh
+#bash-completion
 
-# extras
-htop
-tree
-neovim
-mlocate
-ntfs-3g  # to write to ntfs formatted drives
+### extras
+#htop
+#tree
+#neovim
+#mlocate
+#ntfs-3g  # to write to ntfs formatted drives
 
-# gnome packages
-gdm
-gnome-shell
-gnome-terminal
-networkmanager
-nautilus
+### gnome packages
+#gdm
+#gnome-shell
+#gnome-terminal
+#networkmanager
+#nautilus
 
-# i3 packages
-i3
-dmenu
-rxvt-unicode
-ttf-dejavu
-ttf-font-awesome # for status bar icons
-xorg-server
-xorg-xinit
-xorg-xset
-xorg-xrandr  # for screen configuration
-xorg-xbacklight
-numlockx
-alsa-utils
-polkit  # allows poweroff,reboot,etc. commands without sudo
+### i3 packages
+#i3
+#dmenu
+#rxvt-unicode
+#ttf-dejavu
+#ttf-font-awesome # for status bar icons
+#xorg-server
+#xorg-xinit
+#xorg-xset
+#xorg-xrandr  # for screen configuration
+#xorg-xbacklight
+#numlockx
+#alsa-utils
+#polkit  # allows poweroff,reboot,etc. commands without sudo
 
-# extra gui packages
-gvim  # includes clipboard support (conflicts with vim)
-xsel  # clipboard support for neovim
-firefox
-chromium
+### extra gui packages
+#gvim  # includes clipboard support (conflicts with vim)
+#xsel  # clipboard support for neovim
+#firefox
+#chromium
 #xf86-video-fbdev  # xorg driver for raspberry pi
 
-# laptop stuff
-dialog  # for wifi-menu
-wpa_supplicant
-acpi  # for battery status
-wpa_actiond # auto connect; https://wiki.archlinux.org/index.php/netctl#Wireless
+### laptop stuff
+#dialog  # for wifi-menu
+#wpa_supplicant
+#acpi  # for battery status
+#wpa_actiond # auto connect; https://wiki.archlinux.org/index.php/netctl#Wireless
 
-# retro game stuff
-retroarch
-retroarch-assets-xmb
-retroarch-autoconfig-udev
-libretro-nestopia  # nes core
+### retro game stuff
+#retroarch
+#retroarch-assets-xmb
+#retroarch-autoconfig-udev
+#libretro-nestopia  # nes core
 PACKAGES
 
 # edit the list before installing
