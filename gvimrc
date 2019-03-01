7327f1b553a2512b96e13ed42a0130d194fa26b8
Tyler Brazier <tyler@tylerbrazier.com>
2019-03-01 14:32:21 -0600

    Remove GUI related stuff
    I don't have a need for i3 and it requires too much configuration.
    Gnome settings can be set in the gui itself.
    I'm trying to drastically simplify the dotfiles and cutting all this
    shit out will help a lot.

--- gvimrc
+++
@@ -1,3 +0,0 @@
-" no right and left scrollbars
-set guioptions-=r
-set guioptions-=L
--- scripts/mklinks.sh
+++ scripts/mklinks.sh
@@ -16,21 +16,9 @@ eval $cmd "$(realpath bashrc)" ~/.bashrc
 eval $cmd "$(realpath gitconfig)" ~/.gitconfig
 eval $cmd "$(realpath tmux.conf)" ~/.tmux.conf
 eval $cmd "$(realpath vimrc)" ~/.vimrc
-eval $cmd "$(realpath gvimrc)" ~/.gvimrc
-eval $cmd "$(realpath xinitrc)" ~/.xinitrc
-eval $cmd "$(realpath Xresources)" ~/.Xresources
 
 mkdir -p ~/.vim/plugin/
 eval $cmd "$(realpath vim/plugin/plugs.vim)" ~/.vim/plugin/plugs.vim
 
 mkdir -p ~/.config/nvim/
 eval $cmd "$(realpath config/nvim/init.vim)" ~/.config/nvim/init.vim
-
-mkdir -p ~/.config/gtk-3.0/
-eval $cmd "$(realpath config/gtk-3.0/settings.ini)" ~/.config/gtk-3.0/settings.ini
-
-mkdir -p ~/.config/i3/
-eval $cmd "$(realpath config/i3/config)" ~/.config/i3/config
-
-mkdir -p ~/.config/i3status/
-eval $cmd "$(realpath config/i3status/config)" ~/.config/i3status/config
