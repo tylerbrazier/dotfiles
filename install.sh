72afb060dd7af8e9efeaf19d2968ad2f2082b1c5
Tyler Brazier <tyler@tylerbrazier.com>
2019-03-01 15:08:37 -0600

    install.sh: add overwrite warning

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -e
 
 dotfiles=bash_profile,bashrc,vimrc,gitconfig,tmux.conf
 
+echo "This will OVERWRITE these files in home: $dotfiles"
+read -p "Press Enter to continue, ctrl-c to cancel"
+
 curl -L https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{$dotfiles} -o ~/.#1
 
 # git-prompt.sh is used by bashrc
