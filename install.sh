de27880aafa41679fdd7666eb6110cbef9ba0c47
Tyler Brazier <tyler@tylerbrazier.com>
2020-12-27 21:18:13 -0600

    Don't need tmux yank
    https://github.com/tmux-plugins/tmux-yank#linux
    
    Even without set-clipboard in .tmux.conf, pressing y in copy-mode
    seems to work in Alacritty

--- install.sh
+++ install.sh
@@ -23,11 +23,6 @@ curl -L --create-dirs https://raw.githubusercontent.com/tylerbrazier/dotfiles/ma
 # git-prompt.sh is used by bashrc
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
-if [ ! -d ~/.tmux-yank ]; then
-	git clone --depth 1 https://github.com/tmux-plugins/tmux-yank.git ~/.tmux-yank
-	echo "tmux-yank has additional requirements: https://github.com/tmux-plugins/tmux-yank#requirements"
-fi
-
 # :help packages
 package_dir=~/.vim/pack/x/start
 mkdir -p $package_dir
