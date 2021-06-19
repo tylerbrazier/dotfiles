08222ec159604e60d06eb98d08e86aa9e234ccda
Tyler Brazier <tyler@tylerbrazier.com>
2021-06-18 21:55:37 -0400

    install.sh: better output info

--- install.sh
+++ install.sh
@@ -3,15 +3,18 @@
 # bail on error
 set -e
 
+cd ~
+pwd
 echo '
 bashrc
 gitconfig
 vimrc
-' | xargs -I {} curl https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{} -o ~/.{}
+' | xargs -t -I {} curl -s https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{} -o .{}
 
 # :help packages
 mkdir -p ~/.vim/pack/x/start
 cd ~/.vim/pack/x/start
+pwd
 echo '
 tpope/vim-commentary
 tpope/vim-surround
@@ -20,7 +23,7 @@ tylerbrazier/vim-flintstone
 airblade/vim-gitgutter
 pangloss/vim-javascript
 editorconfig/editorconfig-vim
-' | xargs -I {} git clone --depth 1 https://github.com/{}
+' | xargs -t -I {} git clone -q --depth 1 https://github.com/{}
 
 # To update the plugins:
 #   ls -d ~/.vim/pack/x/start/* | xargs -I {} git -C {} pull
