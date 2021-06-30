54b1254b89212863fada72168d9016bf5d4f2161
Tyler Brazier <tyler@tylerbrazier.com>
2021-06-30 18:12:32 -0500

    install.sh: prompt to confirm curl commands

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ set -e
 
 cd "$HOME"
 echo "cwd: $PWD"
+echo "Type 'y' to confirm curl commands:"
 echo '
 bashrc
 vimrc
 gitconfig
-' | xargs -t -I{} curl -o .{} \
+' | xargs -p -I{} curl -o .{} \
 	https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{}
