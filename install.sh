5592e81ff6a39c9a179afcabe27d24a989a99227
Tyler Brazier <tyler@tylerbrazier.com>
2021-08-31 17:35:51 -0600

    Add yd youtube-dl wrapper script

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ config/sway/config
 config/i3status/config
 config/alacritty.yml
 local/bin/dmenu_history
+local/bin/yd
 ' | xargs -p -I{} curl --create-dirs -o .{} \
 	https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{}
 
 chmod -f +x "$HOME/.local/bin/dmenu_history"
+chmod -f +x "$HOME/.local/bin/yd"
