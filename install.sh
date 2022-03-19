e901af35cb1c7c782c46f21a4875cf4c9128e19d
Tyler Brazier <tyler@tylerbrazier.com>
2022-03-18 21:08:05 -0600

    Add shortcuts/archive_pic

--- install.sh
+++ install.sh
@@ -12,11 +12,13 @@ config/i3status/config
 config/alacritty.yml
 termux/termux.properties
 shortcuts/sshd
+shortcuts/archive_pic
 local/bin/dmenu_history
 local/bin/yd
 ' | xargs -p -I{} curl --create-dirs -o .{} \
 	https://raw.githubusercontent.com/tylerbrazier/dotfiles/master/{}
 
 chmod -f +x "$HOME/.shortcuts/sshd"
+chmod -f +x "$HOME/.shortcuts/archive_pic"
 chmod -f +x "$HOME/.local/bin/dmenu_history"
 chmod -f +x "$HOME/.local/bin/yd"
