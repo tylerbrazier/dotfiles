7c3a28c36aa45a3945a7344e4023bf0b1daad793
Tyler Brazier <tyler@tylerbrazier.com>
2023-12-24 20:56:03 -0600

    install.sh: show diffs when prompting

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ for f in $(git ls-files); do
 	[[ $f =~ readme|install.sh ]] && continue
 
 	if [ $# -eq 0 ]; then
+		if git -P diff "$HOME/.$f" "$f" 2>/dev/null; then
+			echo "$f is up to date."
+			continue
+		fi
+
 		REPLY=
 		until [[ $REPLY =~ [ynYN] ]]; do
 			read -rn1 -p "$f? (y/n) "
