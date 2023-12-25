693d118f8d3898d422ebdd938d2ce04a0abae3be
Tyler Brazier <tyler@tylerbrazier.com>
2023-12-24 20:02:16 -0600

    install.sh: can specify files to install as args

--- install.sh
+++ install.sh
@@ -2,15 +2,21 @@
 
 cd "$(dirname "$0")" || exit
 
-echo "Press y or n:"
 for f in $(git ls-files); do
 	[[ $f =~ readme|install.sh ]] && continue
 
-	REPLY=; until [[ $REPLY =~ [ynYN] ]]; do
-		read -rn1 -p "$f? "; echo
-	done
-
-	[[ $REPLY =~ [nN] ]] && continue
+	if [ $# -eq 0 ]; then
+		REPLY=
+		until [[ $REPLY =~ [ynYN] ]]; do
+			read -rn1 -p "$f? (y/n) "
+			echo
+		done
+		[[ $REPLY =~ [nN] ]] && continue
+	elif [[ $* != *$f* ]]; then
+		continue
+	else
+		echo "Copying $f"
+	fi
 
 	[[ $f == */* ]] && mkdir -p "$HOME/.${f%/*}"
 	cp "$PWD/$f" "$HOME/.$f"
