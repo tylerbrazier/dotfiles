fd274feba7e147b37f1859235fb49c671740aebf
Tyler Brazier <tyler@brazier.us>
2026-07-04 09:09:10 -0500

    plugs: remove gitsigns config & maps
    Use it for signs and renegade for everything else. I wanted to add git
    log functionality to gitsigns but it doesn't look like the maintainer is
    accepting any new features:
    https://github.com/lewis6991/gitsigns.nvim/blob/main/CONTRIBUTING.md
    If I make a PR I wonder if I'd get a response like this:
    https://github.com/lewis6991/gitsigns.nvim/pull/1438#issuecomment-3693312290
    
    So good old renegade it is

--- plugs.lua
+++ plugs.lua
@@ -58,36 +58,3 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 	group = augroup,
 	callback = function(ev) require('lint').try_lint() end
 })
-
-require('gitsigns').setup {
-	signs = {
-		add		= { text = '┃' },
-		change		= { text = '┃' },
-		changedelete	= { text = '┃' },
-		delete		= { text = '_' },
-		topdelete	= { text = '‾' },
-		untracked	= { text = '┆' },
-	},
-	signs_staged = {
-		add		= { text = '┃' },
-		change		= { text = '┃' },
-		changedelete	= { text = '┃' },
-		delete		= { text = '_' },
-		topdelete	= { text = '‾' },
-		untracked	= { text = '┆' },
-	},
-	-- https://github.com/lewis6991/gitsigns.nvim/issues/1344
-	diff_opts = { linematch = 0 },
-}
-
-vim.keymap.set('n', 'gsb', ':Gitsigns blame<CR>')
-vim.keymap.set('n', 'gsq', ':Gitsigns setqflist all<CR>')
-vim.keymap.set('n', 'gsd', ':Gitsigns diffthis <Up>')
-vim.keymap.set('n', 'gsp', ':Gitsigns preview_hunk<CR>')
-vim.keymap.set('n', 'gss', ':Gitsigns stage_hunk<CR>')
-vim.keymap.set('n', 'gsu', ':Gitsigns undo_stage_hunk<CR>')
-vim.keymap.set('n', 'gsS', ':Gitsigns stage_buffer<CR>')
-vim.keymap.set('n', 'gsr', ':Gitsigns reset_hunk<CR>')
-vim.keymap.set('n', 'gsR', ':Gitsigns reset_buffer<CR>')
-vim.keymap.set('n', ']g',  ':Gitsigns nav_hunk next<CR>')
-vim.keymap.set('n', '[g',  ':Gitsigns nav_hunk prev<CR>')
